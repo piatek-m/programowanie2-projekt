3e53e9890936acd636acd7c3dcb3d8b0e3124ea1
JustAFoxxyFox <foxxythefoxowo@gmail.com>
2025-05-21 17:15:32 +0200

    Update IcepickerPhase2.h - parity z Icepickerem

--- IcepickerPhase2.h
+++ IcepickerPhase2.h
@@ -3,14 +3,15 @@
 
 #include "Icepicker.h"
 
-// faza druga, ma mniej zdrowia ale za to nakłada debuff wet
+//faza druga ma mniej zdrowia ale nakłada debuff wet
 class IcepickerPhase2 : public Icepicker {
 public:
     IcepickerPhase2()
-        : Icepicker("Icepicker Meltdown", 20, 12, "heat","wet") {}
+        : Icepicker("Icepicker Meltdown", 20, 12, "heat", "wet") {}
 
-    void phaseChange() override {
-        std::cout << "Icepicker kompletnie się rozpływa!" << std::endl; // tu nic się nie dzieje nie ma kolejnej fazy
+    std::unique_ptr<Icepicker> phaseChange() override {
+        std::cout << "Icepicker kompletnie się rozpływa!" << std::endl;
+        return nullptr; // nie ma już kolejnej fazy więc zwraca nullptr
     }
 };
 
