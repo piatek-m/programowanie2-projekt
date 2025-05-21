9ebdd5376b0536ca458b238fe34bcf0651c907d0
JustAFoxxyFox <foxxythefoxowo@gmail.com>
2025-05-21 16:49:08 +0200

    Uzupełnienie metod - removeEffect, hasEffect i updateEffectTime

--- StatusEffectManager.cpp
+++ StatusEffectManager.cpp
@@ -19,3 +19,50 @@ void StatusEffectManager::addEffect(const StatusEffect &effect)
     // jesli nie jest nałożony to dodaje efekt
     activeEffects.push_back(effect);
 }
+
+void StatusEffectManager::removeEffect(StatusEffectType type)
+{
+    // usuń efekt danego typu
+    activeEffects.erase(
+        std::remove_if(activeEffects.begin(), activeEffects.end(), 
+                       [type](const StatusEffect &e)
+                       {
+                           return e.m_effectType == type;
+                       }),
+        activeEffects.end());
+}
+
+bool StatusEffectManager::hasEffect(StatusEffectType type) const
+{
+    for (const auto &e : activeEffects)
+    {
+        if (e.m_effectType == type) // sprawdza czy obecnie ma jakiś efekt danego typu
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void StatusEffectManager::updateEffectTime(int deltaTime)
+{
+    // sprawdza czasy dla wszystkich aktywnych efektów
+    for (auto &e : activeEffects)
+    {
+        e.remainingDuration -= deltaTime; // zmniejsza pozostały czas o deltaTime
+    }
+
+    // usuwa efekty dla których skończył się czas
+    activeEffects.erase(
+        std::remove_if(activeEffects.begin(), activeEffects.end(), // remove_if zostawia wszystkie aktywne efekty i usuwa te dla których czas się skończył
+                       [](const StatusEffect &e)
+                       {
+                           return e.remainingDuration <= 0;
+                       }),
+        activeEffects.end());
+}
+
+std::vector<StatusEffect> StatusEffectManager::getActiveEffects() const
+{
+    return activeEffects; // zwraca aktywne efekty
+}
