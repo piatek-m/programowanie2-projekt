1e1510cbfba5fae4dfddb5285a33f707ab3a271a
JustAFoxxyFox <foxxythefoxowo@gmail.com>
2025-05-21 17:12:47 +0200

    phaseChange() zwraca nowy obiekt IcepickerPhase2

--- IcepickerPhase1.h
+++ IcepickerPhase1.h
@@ -3,16 +3,15 @@
 
 #include "Icepicker.h"
 #include "IcepickerPhase2.h"
-
-// faza pierwsza, która ma więcej zdrowia ale nie daje żadnych debuffów
+// klasa przeciwnika która ma więcej zdrowia ale nie daje debuffów
 class IcepickerPhase1 : public Icepicker {
 public:
     IcepickerPhase1()
         : Icepicker("Icepicker", 40, 10, "heat") {}
 
-    void phaseChange() override {
-        std::cout << "Icepicker topi się!" << std::endl; // informacja o zmianie fazy
-        return new IcepickerPhase2(); // zwraca obiekt IcepickerPhase2
+    std::unique_ptr<Icepicker> phaseChange() override { 
+        std::cout << "Icepicker topi się i przechodzi do fazy 2!" << std::endl;
+        return std::make_unique<IcepickerPhase2>(); // zwraca nowy obiekt IcepickerPhase2
     }
 };
 
