5dea589185910a85ffac6419bf9b9ba9dd2983c1
Marcin Piątek <piatekmarcin+coding@proton.me>
2025-05-22 10:06:28 +0200

    Dodanie headerów agregacyjnych

---
+++ EnemyAggregator.h
@@ -0,0 +1,12 @@
+#ifndef ENEMYAGGREGATOR_H
+#define ENEMYAGGREGATOR_H
+
+#include "EnemyClasses/Enemy.h"
+#include "EnemyClasses/Ent.h"
+#include "EnemyClasses/Eyesore.h"
+#include "EnemyClasses/Icepicker.h"
+#include "EnemyClasses/IcepickerPhase1.h"
+#include "EnemyClasses/IcepickerPhase2.h"
+#include "EnemyClasses/Lifestealer.h"
+
+#endif
---
+++ StatusEffectAggregator.h
@@ -0,0 +1,7 @@
+#ifndef STATUSEFFECTAGGREGATOR_H
+#define STATUSEFFECTAGGREGATOR_H
+
+#include "StatusEffectClasses/StatusEffect.h"
+#include "StatusEffectClasses/StatusEffectManager.h"
+
+#endif
