d8d28bc4fdcbe956688cefc5c875240d8015a91f
Marcin Piątek <piatekmarcin+coding@proton.me>
2025-05-22 11:48:33 +0200

    Wprowadzenie przechwytywania na Linuxie

--- main.cpp
+++ main.cpp
@@ -1,11 +1,49 @@
-#include "Entity.h"
-#include "Player.h"
-#include "EnemyAggregator.h"
+// #include "Entity.h"
+// #include "Player.h"
+// #include "EnemyAggregator.h"
+#include <ncurses.h>
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
+    initscr();            // Start curses mode
+    keypad(stdscr, TRUE); // Enable arrow keys and function keys
+    noecho();             // Do not print input chars
+    cbreak();             // Disable line buffering
 
+    int c;
+
+    while (1)
+    {
+        c = getch();
+
+        switch (c)
+        {
+        case KEY_UP:
+            printw("Up\n");
+            break;
+        case KEY_DOWN:
+            printw("Down\n");
+            break;
+        case KEY_LEFT:
+            printw("Left\n");
+            break;
+        case KEY_RIGHT:
+            printw("Right\n");
+            break;
+        case 'q':     // Exit on 'q' key
+            endwin(); // Restore terminal settings
+            return 0;
+        default:
+            printw("Not arrow: %c\n", c);
+            break;
+        }
+
+        refresh(); // Print to screen
+    }
+
+    endwin(); // Should not be reached, but good practice
     return 0;
 }
