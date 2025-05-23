c4083c08b678e9bbaefa53a28cb6f25daa733fbe
JustAFoxxyFox <foxxythefoxowo@gmail.com>
2025-05-23 17:42:41 +0200

    Dodanie klasy dla przedmiotów nakładających debuff

---
+++ StatusEffectItem.h
@@ -0,0 +1,24 @@
+#ifndef STATUSEFFECTITEM_H
+#define STATUSEFFECTITEM_H
+
+#include "Item.h"
+#include "StatusEffect.h"
+
+// Klasa pośrednia dla itemów nakładających debuffy
+class StatusEffectItem : public Item {
+protected:
+    StatusEffectType m_effectType;
+    int m_duration;
+
+public:
+    StatusEffectItem(const std::string& iname, StatusEffectType effectType, int duration)
+        : Item(iname), m_effectType(effectType), m_duration(duration) {}
+
+    virtual std::string getItemType() const override { return "StatusEffect"; }
+
+    StatusEffect getStatusEffect() const {
+        return StatusEffect(m_effectType, m_duration);
+    }
+};
+
+#endif
