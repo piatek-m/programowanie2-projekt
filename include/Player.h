c1868bf77b5300db20d02cc0552b84e562e8a117
Marcin Piątek <piatekmarcin+coding@proton.me>
2025-05-25 16:12:23 +0200

    Teraz metody Inventory przyjmują shared_ptr<Item>, a nie Item

--- include/Player.h
+++ include/Player.h
@@ -2,9 +2,10 @@
 #define PLAYER_H
 
 #include "Entity.h"
+#include "Item.h"
+#include <memory> // shared_ptr w wektorze items
 #include <string>
 #include <vector>
-#include "Item.h"
 
 // klasa gracza
 class Player : public Entity
@@ -26,13 +27,13 @@ public:
         std::vector<std::shared_ptr<Item>> m_items; // smart pointer który pozwala na przechowywanie różnych podklas w wektorze
 
     public:
-        void addItem(const Item &item); // dodawanie przedmiotu do ekwipunku
-        void removeItem(const Item &item); // usuwanie przedmiotu z ekwipunku np. przy użyciu
-        bool hasItem(const Item &item) const; // sprawdza czy przedmiot jest w ekwipunku
-        void listItems() const; // wypisuje wszystkie przedmioty, które gracz obecnie posiada
+        void addItem(const std::shared_ptr<Item> &item);       // dodawanie przedmiotu do ekwipunku
+        void removeItem(const std::shared_ptr<Item> &item);    // usuwanie przedmiotu z ekwipunku np. przy użyciu
+        bool hasItem(const std::shared_ptr<Item> &item) const; // sprawdza czy przedmiot jest w ekwipunku
+        void listItems() const;                                // wypisuje wszystkie przedmioty, które gracz obecnie posiada
     };
 
-    Inventory& getInventory(); // dostęp do ekwipunku
+    Inventory &getInventory(); // dostęp do ekwipunku
 
 private:
     Inventory m_inventory; // prywatny ekwipunek dla gracza
