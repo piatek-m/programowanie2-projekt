5095dfdf3775d7e54a3eae4cdd6cb69766233354
JustAFoxxyFox <foxxythefoxowo@gmail.com>
2025-05-27 21:05:41 +0200

    illumination

--- include/Eyesore.h
+++ include/Eyesore.h
@@ -10,6 +10,17 @@ class Eyesore : public Enemy
 public:
     // konstruktor tworzący klasę o nazwie Eyesore
     Eyesore(std::string cl_Name, int hp, int MAX_HP, StatusEffectType weak, StatusEffectType debuff) : Enemy("Eyesore", 40, 40, StatusEffectType::illumination, StatusEffectType::confusion) {};
+     
+    void applyIlluminationEffect() {
+        // sprawdzenie czy ma efekt illumination
+        for (const auto& effect : m_statusEffects) {
+            if (effect.m_effectType == StatusEffectType::illumination) {
+                    int damage = 5; 
+                    takeDamage(damage);
+                }
+                break;
+            }
+        }
 };
 
 #endif
